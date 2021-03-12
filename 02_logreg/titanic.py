3277b6a214df8f39f386fd0c7cd3c363e4ad591a
Ben Cook <bfc782@hotmail.com>
2021-03-12 17:56:03 +0100

    confusion, recall_v_prec, roc

--- 02_logreg/titanic.py
+++ 02_logreg/titanic.py
@@ -1,3 +1,4 @@
+#%%
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -6,10 +7,11 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import MinMaxScaler, StandardScaler, FunctionTransformer, OneHotEncoder
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, cross_val_predict
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.metrics import plot_confusion_matrix, f1_score, precision_score, recall_score, precision_recall_curve, roc_curve
 
 #plt.rcParams('figure.figsize') = (12,6)
 
@@ -27,12 +29,13 @@ train = 'train.csv'
 test = 'test.csv'
 df_train = pd.read_csv(data+train)
 df_test = pd.read_csv(data+test)
-
+#%%
 #EDA shows very few Embarked NaN - choosing to drop these for the moment.
 df_train = df_train.dropna(subset=['Embarked'])
 df_test = df_test.dropna(subset=['Embarked'])
 
-df_train_num = df_train.drop(['Name','Sex','Ticket','Cabin','Embarked'],axis=1)
+df_train_target = df_train['Survived']
+df_train_num = df_train.drop(['Name','Survived','Sex','Ticket','Cabin','Embarked'],axis=1)
 df_train_num.name = 'numerical_data'
 
 #NOTE: at this point we consider how to treat the numerical NaNs - most appear on age 
@@ -50,8 +53,8 @@ df_train_cat.name = 'categorical_data'
 #nan_rows(df_train_cat) #output to eda folder - most are for cabin, some are for embarked
 
 #Encode fe/male assignments to binary gender assignments (though a modern statistical heuristic may be available for non-binary)
-gender = {'female':0, 'male':1}
-df_train_cat['Sex']=df_train_cat['Sex'].map(gender)
+# gender = {'female':0, 'male':1}
+# df_train_cat['Sex']=df_train_cat['Sex'].map(gender)
 
 #One-hot-encode the embarcation points 
 ##df_name_emb = df_train_cat[['Name','Embarked']]
@@ -62,7 +65,7 @@ emb_cat.name = 'emb_cat_now'
 #print(nan_rows(emb_cat))
 cat_encoder = OneHotEncoder()
 emb_cat_1hot = cat_encoder.fit_transform(emb_cat)
-
+#%%
 num_pipeline = Pipeline([
     ('imputer', SimpleImputer(strategy='median')),
     ('std_scaler', StandardScaler()),
@@ -70,13 +73,68 @@ num_pipeline = Pipeline([
 
 #df_train_num_tr = num_pipeline.fit_transform(df_train_num)
 num_attribs = list(df_train_num)
-cat_pass = ['Sex']
-cat_encod = ['Embarkation']
-
+sex_enc = ['Sex']
+emb_enc = ['Embarked']
+#%%
 full_pipeline = ColumnTransformer([
     ('num', num_pipeline, num_attribs),
-    ('cat_pass', 'pass through', cat_pass),
-    ('cat_encod', 'OneHotEncoder(), cat_encod),
+    ('sex', OneHotEncoder(), sex_enc),
+    ('emb', OneHotEncoder(), emb_enc),
   ])
 
-df_train_prepared = full_pipeline.fit_transform(df_train)
+X_train = full_pipeline.fit_transform(df_train)
+y_train = df_train_target
+
+# %%
+m = LogisticRegression(random_state=337)
+m.fit(X_train, y_train)
+m.score(X_train, y_train)
+
+# %%
+cross_val_acc = cross_val_score(estimator=m
+                , X=X_train
+                , y=y_train
+                , cv=5
+                , scoring='accuracy')
+cross_val_acc
+# %%
+plot_confusion_matrix(estimator=m,
+                      X=X_train,
+                      y_true=y_train,
+                      normalize=None)
+# %%
+
+print(f'precision for survival predict:{round(precision_score(y_train, m.predict(X_train), pos_label=1),2)}')
+
+print(f'recall for survival predict:{round(recall_score(y_train, m.predict(X_train), pos_label=1),2)}')
+
+print(f'f1_score for survival predict:{round(f1_score(y_train, m.predict(X_train), pos_label=1),2)}')
+
+# %%
+
+print(f'precision for perished predict:{round(precision_score(y_train, m.predict(X_train), pos_label=0),2)}')
+
+print(f'recall for perished predict:{round(recall_score(y_train, m.predict(X_train), pos_label=0),2)}')
+
+print(f'f1_score for perished predict:{round(f1_score(y_train, m.predict(X_train), pos_label=0),2)}')
+
+# %%
+y_scores = cross_val_predict(m, X_train, y_train, cv=5, method='decision_function')
+precisions, recalls, thresholds = precision_recall_curve(y_train, y_scores)
+
+def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
+  plt.plot(thresholds, precisions[:-1],"b--", label="Precision")
+  plt.plot(thresholds, recalls[:-1],"g-", label="Recall")
+
+plot_precision_recall_vs_threshold(precisions, recalls, thresholds)
+plt.show()
+# %%
+fpr, tpr, thresholds = roc_curve(y_train, y_scores)
+
+def plot_roc_curve(fpr, tpr, label=None):
+  plt.plot(fpr, tpr, linewidth=2, label=label)
+  plt.plot([0,1],[0,1],'k--')
+
+plot_roc_curve(fpr, tpr)
+plt.show
+# %%
