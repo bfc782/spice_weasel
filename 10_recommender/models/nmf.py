7f2d7586ae485001a9b875b62d093fd04e28faa8
Ben Cook <bfc782@hotmail.com>
2021-07-14 12:04:03 +0200

    nmf

--- 10_recommender/models/nmf.py
+++ 10_recommender/models/nmf.py
@@ -1,7 +1,9 @@
+#%% 
 import numpy as np 
 from sklearn.decomposition import NMF 
 import pandas as pd 
 
+#dummy ratings: 5 films, 4 users
 data = [
     [5,4,1,1,3],
     [3,2,1,3,1],
@@ -11,21 +13,27 @@ data = [
 columns = ['Titanic', 'Tiffany', 'Terminator', 'Star Trek','Star Wars']
 index = ['Ada', 'Bob', 'Steve','Margaret']
 
+#create dataframe
 R = pd.DataFrame(data,index=index, columns=columns).values
 
+#create model and set hyperparameters
+#model assumes R - PQ
 model = NMF(n_components=3,init='random',random_state=10)
 
 model.fit(R)
 
-Q=model.components_
+Q=model.components_ #movie-genre matrix
 
-P=model.transform(R)
+P=model.transform(R) # user-genre matrix
 
-print(model.reconstruction_err_)
+print(model.reconstruction_err_) # reconstruction error
 
 nR = np.dot(P,Q)
-print(nR)
+print(nR) # reconstructed matrix!
 
+# a new data point to predict
 query = [[1,2,5,4,5]]
 
+# in
 print(model.transform(query))
+# %%
---
+++ 10_recommender/models/nmf_pb.py
@@ -0,0 +1,11 @@
+#%% imports
+import numpy as np 
+from sklearn.decomposition import NMF 
+import pandas as pd 
+
+#%%
+# data: populate dummy for now
+## could be most active
+data = np.zeros((100,1),dtype=int)
+# %%
+
